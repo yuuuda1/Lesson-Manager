0d6c2e933665ba6d170b276542e7417e6a198996
kawaken1125 <kawakenn1125@yahoo.co.jp>
2018-03-03 22:08:18 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Lesson from './Lesson';
+import Home from './Home';
 
-ReactDOM.render(<Lesson />, document.getElementById('root'));
+ReactDOM.render(<Home />,
+document.getElementById('root'));
