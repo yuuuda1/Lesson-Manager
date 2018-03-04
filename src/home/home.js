c6f03f3d0a64cf854d52dcc13fce0f8168617a40
makeupwith <g1644303@cc.kyoto-su.ac.jp>
2018-03-04 19:57:13 +0900

    Add HomePage Contnet

---
+++ src/home/home.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react'
+import {withStyles} from 'material-ui/styles'
+import Container from './Container'
+
+const drawerWidth = 56
+
+const styles = () => ({
+  root: {
+    marginLeft: drawerWidth,
+    marginTop: '64px',
+    zIndex: '1000'
+  },
+  mainTitle: {
+    textAlign: 'center',
+    marginTop: '176px',
+    fontSize: '56px',
+    color: '#000000',
+    opacity: '0.87'
+  },
+  topSubTitle: {
+    textAlign: 'center',
+    fontSize: '34px',
+    marginTop: '14px',
+    color: '#000000',
+    opacity: '0.54'
+  },
+  bottomSubTitle: {
+    textAlign: 'center',
+    fontSize: '45px',
+    marginTop: '261px',
+    color: '#000000',
+    opacity: '0.54'
+  }
+})
+
+class Home extends Component {
+  render() {
+    const {
+      classes,
+      ...other
+    } = this.props
+    return(
+       <div className={classes.root}>
+         <div {...other}>
+           <Container />
+         </div>
+         <div className={classes.mainTitle}>
+           <Typography>Lesson-Manager</Typography>
+         </div>
+         <div className={classes.topSubTitle}>
+           <Typography>This application can manage your timetable</Typography>
+         </div>
+         <div className={classes.bottomSubTitle}>
+           <Typography>"Let's start manage"</Typography>
+         </div>
+       </div>
+    )
+  }
+}
+export default withStyles(styles)(Home)
