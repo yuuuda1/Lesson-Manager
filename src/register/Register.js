ef8a5f967292cd9c760e204a6ef4867141c31344
kawaken1125 <kawakenn1125@yahoo.co.jp>
2018-03-04 21:51:59 +0900

    make register page

--- src/register/Register.js
+++ src/register/Register.js
@@ -6,11 +6,27 @@ import SearchButton from './searchButton'
 
 const styles = () => ({
   root: {
-    
-
   },
   title: {
-    textAlign: 'center'
+    marginLeft: '110px',
+    textAlign: 'center',
+    fontSize: '16px',
+    color: '#000000',
+    opacity: '0.87',
+    fontFamily: 'Roboto',
+    
+    
+  },
+  searchButton: {
+    marginLeft: '420px',
+    marginBottom: '64px',
+    marginTop: '20px',
+  },
+  registerContent: {
+    width: '100%',
+    height: '100%',
+    marginLeft: '70px',
+    marginTop: '60px'
   }
 
 })
@@ -23,12 +39,15 @@ class Register extends Component {
     } = this.props
     return(
       <div className={classes.root} {...other}>
-        
         <Container>
-          <div className={classes.title}>
-            My時間割の新規登録
+          <div className={classes.registerContent}>
+            <div className={classes.title}>
+              My時間割の新規登録
+            </div>
+            <SearchButton className={classes.searchButton}
+            />
+            <LessonList />
           </div>
-          <LessonList />
         </Container>
       </div>
     )
