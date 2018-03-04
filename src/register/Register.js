2cdae35d7f3a3da9bcfd7760cb6ea520c2971fc1
makeupwith <g1644303@cc.kyoto-su.ac.jp>
2018-03-04 18:55:37 +0900

    Fixed Content

---
+++ src/register/Register.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { withStyles } from 'material-ui/styles'
+import Container from './Container'
+import LessonList from './LessonList'
+
+const styles = () => {
+  root: {
+
+  }
+}
+
+class Register extends Component {
+  render() {
+    const {
+      classes,
+      ...other
+    } = this.props
+    return(
+      <div className={classes.root} {...other}>
+        <Container>
+          <LessonList />
+        </Container>
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(Register)
