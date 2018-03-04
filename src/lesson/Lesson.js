719b4eafe9f88cf8298c6fe29ff24fc02db29d9b
makeupwith <g1644303@cc.kyoto-su.ac.jp>
2018-03-04 18:45:47 +0900

    Created lesson directory

---
+++ src/lesson/Lesson.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { withStyles } from 'material-ui/styles'
+import Container from '../Container.js'
+import TimeTable from './TimeTable.js'
+
+const styles = () => ({
+  root: {
+    backgroundColor: '#FAFAFA',
+    position: 'absolute',
+    top: '0px',
+    left: '0px',
+    width: '100%',
+    height: '100vh'
+  }
+})
+
+class Lesson extends Component {
+  render() {
+    const {
+      classes,
+      ...other
+    } = this.props
+    return(
+      <div className={classes.root} {...other}>
+        <Container>
+          <TimeTable />
+        </Container>
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(Lesson)
