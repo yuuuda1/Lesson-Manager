2954516318368a1155bdd826d16fb5a6ac98ebc2
kawaken1125 <kawakenn1125@yahoo.co.jp>
2018-03-04 19:46:25 +0900

    add title

--- src/register/Register.js
+++ src/register/Register.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react'
 import { withStyles } from 'material-ui/styles'
 import Container from '../Container'
 import LessonList from './LessonList'
+import SearchButton from './searchButton'
 
-const styles = () => {
+const styles = () => ({
   root: {
+    
 
+  },
+  title: {
+    textAlign: 'center'
   }
-}
+
+})
 
 class Register extends Component {
   render() {
@@ -17,7 +23,11 @@ class Register extends Component {
     } = this.props
     return(
       <div className={classes.root} {...other}>
+        
         <Container>
+          <div className={classes.title}>
+            My時間割の新規登録
+          </div>
           <LessonList />
         </Container>
       </div>
