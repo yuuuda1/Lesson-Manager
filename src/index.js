42a723311fabb192f8461573266eefea7f11bd61
makeupwith <g1644303@cc.kyoto-su.ac.jp>
2018-03-04 18:44:47 +0900

    Fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Lesson from './Lesson';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Lesson from './lesson/Lesson'
 
-ReactDOM.render(<Lesson />, document.getElementById('root'));
+ReactDOM.render(<Lesson />, document.getElementById('root'))
