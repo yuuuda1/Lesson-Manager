7f4eb2c0bd065c5472d8b977c7ac893cd4727c81
makeupwith <g1644303@cc.kyoto-su.ac.jp>
2018-03-05 05:10:22 +0900

    Created theme.js

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from 'material-ui/styles'
+import cyan from 'material-ui/colors/cyan'
+import red from 'material-ui/colors/red'
+import orange from 'material-ui/colors/orange'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: cyan,
+    secondary: {
+      ...orange,
+      A200: '#FFAB40',
+    },
+    error: red
+  },
+})
+
+export default theme
