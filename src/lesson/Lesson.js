c02897d345ea7460eaec1d9dbe004709515bbbd3
shoMMKS <sho15-kanan-town-1739@ezweb.ne.jp>
2018-03-05 19:38:25 +0900

    abort

--- src/lesson/Lesson.js
+++ src/lesson/Lesson.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { withStyles } from 'material-ui/styles'
+import Button from 'material-ui/Button'
 import Container from '../Container.js'
 import TimeTable from './TimeTable.js'
 
@@ -15,18 +16,63 @@ const styles = () => ({
 })
 
 class Lesson extends Component {
-  ComponentDidMount() {
-    this.props.requestAllLessons()
+  // eslint-disable-next-line
+  constructor(props) {
+    super(props)
+  }
+  
+  componentDidMount() {
+    console.log('chk')
+    //this.props.requestAllLessons()
+  }
+  
+  getL = () => {
+    this.props.requestAllLessons('理工学実験')
+  }
+
+  getT = () => {
+    this.props.requestAllTimetables()
+  }
+
+  show = () => {
+    this.props.requestGetTimetable(4)
+  }
+
+  post = () => {
+    this.props.requestPostTimetables({ lessons: [12, 365, 7633] })
+  }
+
+  put = () => {
+    this.props.requestPutTimetables(2, { lessons: [33, 66, 99] })
+  }
+
+  delete = () => {
+    this.props.requestDeleteTimetables(1)
   }
   render() {
     const {
       classes,
+      lessons,
+      requestAllLessons,
+      requestAllTimetables,
+      requestGetTimetable,
+      requestPostTimetables,
+      requestPutTimetables,
+      requestDeleteTimetables,
       ...other
     } = this.props
     return(
       <div className={classes.root} {...other}>
         <Container>
-          <TimeTable />
+          <div>
+            <TimeTable />
+            <Button onClick={this.getL}>getL</Button>
+            <Button onClick={this.getT}>getT</Button>
+            <Button onClick={this.show}>show</Button>
+            <Button onClick={this.post}>post</Button>
+            <Button onClick={this.put}>put</Button>
+            <Button onClick={this.delete}>delete</Button>
+          </div>
         </Container>
       </div>
     )
