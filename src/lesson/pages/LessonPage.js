cc8050a6e0ffda1327741efa57dd35058768e190
makeupwith <g1644303@cc.kyoto-su.ac.jp>
2018-04-17 10:06:26 +0900

    Fix title positioin

--- src/lesson/pages/LessonPage.js
+++ src/lesson/pages/LessonPage.js
@@ -2,32 +2,38 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from 'material-ui/styles'
 import Typography from 'material-ui/Typography'
+import Button from 'material-ui/Button'
 import Container from 'app/Container'
-import { Link } from 'react-router-dom'
 import TimeTable from './components/TimeTable'
+import { Link } from 'react-router-dom'
 
 const styles = () => ({
   root: {
     backgroundColor: '#FAFAFA',
-    position: 'absolute',
-    top: '0px',
-    left: '0px',
     width: '100%',
-    height: '100vh'
+    height: '100%'
   },
   title: {
+    maxWidth: '1150px',
     marginLeft: '32px',
     marginTop: '32px',
     paddingBottom: '8px',
     borderBottom: '1px solid #000000',
     borderStyle: '0.54',
-    fontSize: '24px',
-    fontWeight: 'bold',
-    color: '#000000',
+    fontWeight: 'Regular',
     opacity: '0.54'
   },
-  universal: {
-    marginBottom: '8px'
+  titleStyle: {
+    height: '32px',
+    display: 'inline-block',
+    fontSize: '20px',
+    paddingTop: '8px'
+  },
+  buttonStyle: {
+    height: '36px',
+    fontSize: '16px',
+    display: 'inline-block',
+    float: 'right'
   }
 })
 
@@ -54,26 +60,16 @@ class LessonPage extends Component {
       <div className={classes.root} {...other}>
         <Container>
           <div className={classes.title}>
-            <Typography variant='title'>
+            <Typography variant='title' className={classes.titleStyle}>
               MY時間割
             </Typography>
             <Link to='/users/timetables/edit'>
-              <Typography>
-                編集する
-              </Typography>
+              <Button className={classes.buttonStyle} color='defalut'>
+                編集
+              </Button>
             </Link>
           </div>
           <div>
-            {myTimetable}
-          </div>
-          <div className={classes.title}>
-            <Typography className={classes.universal} variant='title'>
-              京都産業大学 コンピュータ理工学部 インテリジェントシステム学科
-            </Typography>
-            <Typography variant='title'>
-              平成28年度 秋学期 時間割
-              {' '}
-            </Typography>
           </div>
           {myTimetable}
         </Container>
