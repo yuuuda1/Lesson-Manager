a9372a62c8fb80a78ebb95295da9917b2ab44c64
makeupwith <g1644303@cc.kyoto-su.ac.jp>
2018-04-17 09:55:44 +0900

    Change font size and title position

--- src/home/home.js
+++ src/home/home.js
@@ -7,34 +7,34 @@ import { Link } from 'react-router-dom'
 
 const styles = () => ({
   root: {
+    fontFamily: 'Roboto',
     width: '100%',
     height: '100%',
-    position: 'absolute',
-    top: '0px',
-    left: '0px',
-    fontFamily: 'Roboto'
   },
   titleBox: {
     width: '100%',
-    height: '240px',
+    height: 'calc(55vh - 240px)',
     backgroundColor: '#00BCD4',
-    paddingTop: '200px'
+    paddingTop: '240px',
   },
   subTitleBox: {
     width: '100%',
-    height: '240px',
-    paddingTop: '80px',
-    backgroundColor: '#FAFAFA'
+    height: 'calc(45vh - 64px)',
+    backgroundColor: '#FAFAFA',
+    paddingTop: '64px'
   },
   projectName: {
     textAlign: 'center',
-    fontSize: '56px',
-    fontWeight: 'bold',
-    color: '#FFF'
+    fontSize: '24px',
+    fontFamily: 'Roboto',
+    fontWeight: 'Regular',
+    color: '#FFF',
+    marginBottom: '24px'
   },
   projectContent: {
     textAlign: 'center',
-    fontSize: '34px',
+    fontSize: '20px',
+    fontFamily: 'Roboto',
     fontWeight: 'Regular',
     marginTop: '0px',
     color: '#000000',
@@ -42,28 +42,30 @@ const styles = () => ({
   },
   guessTitle: {
     textAlign: 'center',
-    fontSize: '45px',
+    fontSize: '20px',
+    fontFamily: 'Roboto',
+    fontWeight: 'Regular',
     color: '#000000',
     opacity: '0.54'
   },
   buttons: {
-    width: '360px',
+    width: '204px',
     position: 'relative',
     margin: 'auto',
-    marginTop: '40px',
+    marginTop: '32px',
     color: '#FFF'
   },
   registerButton: {
-    width: '160px',
-    height: '45px',
+    width: '90px',
+    height: '36px',
     textAlign: 'center',
-    marginRight: '40px',
+    marginRight: '24px',
     color: '#FFF',
     backgroundColor: '#00BCD4'
   },
   loginButton: {
-    width: '160px',
-    height: '45px',
+    width: '90px',
+    height: '36px',
     textAlign: 'center',
     color: '#FFF',
     backgroundColor: '#00BCD4'
@@ -74,25 +76,25 @@ const Home = ({ classes, ...other }) => (
   <div className={classes.root} {...other}>
     <div className={classes.titleBox}>
       <Typography className={classes.projectName}>
-          Lesson-Manager
+        Lesson Manager
       </Typography>
       <Typography className={classes.projectContent}>
-          This application can manage your timetable
+        This application can manage your timetable
       </Typography>
     </div>
     <div className={classes.subTitleBox}>
       <Typography className={classes.guessTitle}>
-          Let&rsquo;s start manage!!
+        Let&rsquo;s start manage!!
       </Typography>
       <div className={classes.buttons}>
         <Link to='/signup'>
           <Button className={classes.registerButton}>
-              SIGN UP
+            SIGN UP
           </Button>
         </Link>
         <Link to='/login'>
           <Button className={classes.loginButton}>
-              LOGIN
+            LOGIN
           </Button>
         </Link>
       </div>
