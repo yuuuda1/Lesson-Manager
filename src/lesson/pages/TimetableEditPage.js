62340cf8f9c3a7e8e639dda5134e51459e3f2689
makeupwith <g1644303@cc.kyoto-su.ac.jp>
2018-04-17 10:10:23 +0900

    Fix style

--- src/lesson/pages/TimetableEditPage.js
+++ src/lesson/pages/TimetableEditPage.js
@@ -1,58 +1,28 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from 'material-ui/styles'
-import Input from 'material-ui-icons/Input'
-import Button from 'material-ui/Button'
 import Typography from 'material-ui/Typography'
 import Container from 'app/Container'
 import LessonList from './components/LessonList'
 
 const styles = () => ({
   root: {
-    position: 'absolute',
-    zIndex: 1,
     width: '100%',
     height: '100%',
-    backgroundColor: '#FAFAFA'
-  },
-  getButton: {
-    color: '#00BCD4',
-    fontSize: '18px',
-    marginTop: '32px'
-  },
-  maintitle: {
-    position: 'absolute',
-    left: '500px',
-    textAlign: 'center',
-    fontSize: '34px',
-    color: '#000000',
-    opacity: '0.87',
-    fontFamily: 'Roboto'
-  },
-  searchButton: {
-    marginLeft: '420px',
-    marginBottom: '64px',
-    marginTop: '20px'
   },
   registerContent: {
     width: '100%',
-    marginLeft: '70px',
-    marginTop: '60px'
+    marginTop: '48px'
   },
   title: {
-    position: 'relative',
-    left: '180px',
-    textAlign: 'center',
+    maxWidth: '200px',
+    margin: 'auto',
     fontSize: '24px',
+    fontFamily: 'Roboto',
+    fontWeight: 'Regular',
     fontColor: '#000000',
     opacity: '0.87'
-  },
-  button: {
-  },
-  icon: {
-    marginLeft: '5px'
   }
-
 })
 
 
@@ -114,16 +84,6 @@ class TimetableEditPage extends Component {
               onSearch={this.handleSearch}
               registedLessons={timetable.lessons}
             />
-            <div className={classes.button}>
-              <Button
-                classes={{ colorInherit:classes.colorInherit }}
-                className={classes.getButton}
-                onClick={this.handleRegister}
-              >
-                  編集する
-                <Input className={classes.icon} />
-              </Button>
-            </div>
           </div>
         </Container>
       </div>
